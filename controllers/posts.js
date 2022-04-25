ca28c4692d9baef39d6b5bf5b65d3f11cf040956
Hank-Ad2-MBP16 <hank@ad2iction.com>
2022-04-25 16:00:03 +0800

    refactor: 提出 controllers

---
+++ controllers/posts.js
@@ -0,0 +1,95 @@
+const PostModel = require('../model/PostModel');
+const handleError = require('../service/handleError');
+const handleSuccess = require('../service/handleSuccess');
+const post = {
+  /**
+   * 查詢所有資料
+   * Doc:https://mongoosejs.com/docs/api/model.html#model_Model.find
+   * @param {Object} param
+   */
+  async getPosts({ req, res }) {
+    const allPost = await PostModel.find();
+    handleSuccess(res, allPost);
+  },
+  /**
+   * 新增單筆資料
+   * Doc:https://mongoosejs.com/docs/api/model.html#model_Model.create
+   * @param {Object} param
+   */
+  async createPost({ req, res, body }) {
+    try {
+      const data = JSON.parse(body);
+      data.content = data.content?.trim(); // 頭尾去空白
+      if (!data.content) throw new Error('[新增失敗] content 未填寫');
+      // 只開放新增 name tags type image conent
+      const newPost = await PostModel.create({
+        name: data.name,
+        tags: data.tags,
+        type: data.type,
+        image: data.image,
+        content: data.content,
+      });
+      handleSuccess(res, newPost);
+    } catch (err) {
+      handleError(res, err);
+    }
+  },
+  /**
+   * 刪除所有資料
+   * Doc:https://mongoosejs.com/docs/api/model.html#model_Model.deleteMany
+   * @param {Object} param
+   */
+  async deletePosts({ req, res }) {
+    await PostModel.deleteMany({});
+    handleSuccess(res, []);
+  },
+  /**
+   * 刪除單筆資料
+   * Doc:https://mongoosejs.com/docs/api/model.html#model_Model.findByIdAndDelete
+   * @param {Object} param
+   */
+  async deletePostById({ req, res }) {
+    try {
+      const id = req.url.split('/').pop();
+      const deletePostById = await PostModel.findByIdAndDelete(id);
+      if (!deletePostById) throw new Error('[刪除失敗] 沒有此 id');
+      handleSuccess(res, deletePostById);
+    } catch (err) {
+      handleError(res, err);
+    }
+  },
+  /**
+   * 修改單筆資料
+   * Doc:https://mongoosejs.com/docs/api/model.html#model_Model.findByIdAndUpdate
+   * @param {Object} param
+   */
+  async updatePostById({ req, res, body }) {
+    try {
+      const id = req.url.split('/').pop();
+      const data = JSON.parse(body);
+      data.content = data.content?.trim(); // 頭尾去空白
+      if (data.name) throw new Error('[修改失敗] 不可修改 name');
+      if (!data.content) throw new Error('[修改失敗] content 未填寫');
+      // 只開放修改 tags type image conent (name 不可改)
+      const updatePostById = await PostModel.findByIdAndUpdate(
+        id,
+        {
+          tags: data.tags,
+          type: data.type,
+          image: data.image,
+          content: data.content,
+        },
+        {
+          // 加這行才會返回更新後的資料，否則為更新前的資料。
+          returnDocument: 'after',
+          // update 相關語法預設 runValidators: false，需手動設寪 true。Doc:https://mongoosejs.com/docs/validation.html#update-validators
+          runValidators: true,
+        }
+      );
+      handleSuccess(res, updatePostById);
+    } catch (err) {
+      handleError(res, err);
+    }
+  },
+};
+module.exports = post;
--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 const http = require('http');
 const mongoose = require('mongoose');
-const PostModel = require('./model/PostModel');
 const dotenv = require('dotenv');
-const handleError = require('./service/handleError');
-const handleSuccess = require('./service/handleSuccess');
-const headers = require('./service/headers');
-const errorHandler = require('./service/handleError');
-const successHandler = require('./service/handleSuccess');
+const HttpControllers = require('./controllers/http');
+const PostsControllers = require('./controllers/posts');
 dotenv.config({ path: './config.env' });
 // DB_CONN 字串若要連到 Mongo Cloud 需登入該網站選擇 Database => Connect => Connect your application 取得
 const dbConn = process.env.DB_CONN.replace(
@@ -29,81 +25,21 @@ const requestListener = async (req, res) => {
   });
 
   if (req.url === '/posts' && req.method === 'GET') {
-    // 查詢所有資料 doc:https://mongoosejs.com/docs/api/model.html#model_Model.find
-    const allPost = await PostModel.find();
-    handleSuccess(res, allPost);
+    PostsControllers.getPosts({ req, res });
   } else if (req.url === '/posts' && req.method === 'POST') {
-    // 新增單筆資料 doc:https://mongoosejs.com/docs/api/model.html#model_Model.create
-    req.on('end', async () => {
-      try {
-        const data = JSON.parse(body);
-        data.content = data.content?.trim();
-        if (!data.content) throw new Error('[新增失敗] content 未填寫');
-        // 只開放新增 name tags type image conent
-        const newPost = await PostModel.create({
-          name: data.name,
-          tags: data.tags,
-          type: data.type,
-          image: data.image,
-          content: data.content,
-        });
-        handleSuccess(res, newPost);
-      } catch (err) {
-        handleError(res, err);
-      }
-    });
+    req.on('end', () => PostsControllers.createPost({ req, res, body }));
   } else if (req.url === '/posts' && req.method === 'DELETE') {
-    // 刪除所有資料 doc:https://mongoosejs.com/docs/api/model.html#model_Model.deleteMany
-    await PostModel.deleteMany({});
-    handleSuccess(res, []);
+    PostsControllers.deletePosts({ req, res });
   } else if (req.url.startsWith('/posts/') && req.method === 'DELETE') {
-    try {
-      // 刪除單筆資料 doc:https://mongoosejs.com/docs/api/model.html#model_Model.findByIdAndDelete
-      const id = req.url.split('/').pop();
-      const deletePostById = await PostModel.findByIdAndDelete(id);
-      if (!deletePostById) throw new Error('[刪除失敗] 沒有此 id');
-      handleSuccess(res, deletePostById);
-    } catch (err) {
-      errorHandler(res, err);
-    }
+    PostsControllers.deletePostById({ req, res });
   } else if (req.url.startsWith('/posts/') && req.method === 'PATCH') {
-    // 修改單筆資料 doc:https://mongoosejs.com/docs/api/model.html#model_Model.findByIdAndUpdate
-    req.on('end', async () => {
-      try {
-        const id = req.url.split('/').pop();
-        const data = JSON.parse(body);
-        if (data.name) throw new Error('[修改失敗] 不可修改 name');
-        if (!data.content) throw new Error('[修改失敗] content 未填寫');
-        // 只開放修改 tags type image conent (name 不可改)
-        const updatePostById = await PostModel.findByIdAndUpdate(
-          id,
-          {
-            tags: data.tags,
-            type: data.type,
-            image: data.image,
-            content: data.content,
-          },
-          {
-            returnDocument: 'after', // 加這行才會返回更新後的資料，否則為更新前的資料。
-          }
-        );
-        successHandler(res, updatePostById);
-      } catch (err) {
-        errorHandler(res, err);
-      }
-    });
-  } else if (req.method == 'OPTIONS') {
-    res.writeHead(200, headers);
-    res.end();
-  } else {
-    res.writeHead(404, headers);
-    res.write(
-      JSON.stringify({
-        status: 'false',
-        message: '無此網站路由',
-      })
+    req.on('end', async () =>
+      PostsControllers.updatePostById({ req, res, body })
     );
-    res.end();
+  } else if (req.method == 'OPTIONS') {
+    HttpControllers.cors(req, res);
+  } else {
+    HttpControllers.notFound(req, res);
   }
 };
 
